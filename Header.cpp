d3ed62bcde16b4edc80ccdf1755a1f980941d5b6
JawadAli1133 <120698315+JawadAli1133@users.noreply.github.com>
2022-12-16 03:58:12 -0800

    Add files via upload

---
+++ Header.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+   cout <<" ******************************************************************************************"<<endl;
+   cout <<" *                                                                                        *"<<endl;
+   cout <<" *                                                                                        *"<<endl;
+   cout <<" *                               Jawad ALi Amajd                                          *"<<endl;
+   cout <<" *                                 Section D                                              *"<<endl;
+   cout <<" *                                Roll no: 20                                             *"<<endl;
+   cout <<" *                                                                                        *"<<endl;
+   cout <<" *                                                                                        *"<<endl;
+   cout <<" *                                                                                        *"<<endl;
+   cout <<" *                                                                                        *"<<endl;
+   cout <<" *                                                                                        *"<<endl;
+   cout <<" ******************************************************************************************"; 
+
+
+}
---
+++ Name.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+main(){
+  cout<<"My Name is Jawad"<<endl<<"My roll no is 183"<<endl<<"Section:D";
+
+}
