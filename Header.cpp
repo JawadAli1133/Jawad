f48c7a7e56d3b7a9f3989029d0b21ec42f7968fb
JawadAli1133 <120698315+JawadAli1133@users.noreply.github.com>
2022-12-18 06:32:35 -0800

    Add files via upload

--- Header.cpp
+++ Header.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 main(){
-   cout <<" ******************************************************************************************"<<endl;
-   cout <<" *                                                                                        *"<<endl;
-   cout <<" *                                                                                        *"<<endl;
-   cout <<" *                               Jawad ALi Amajd                                          *"<<endl;
-   cout <<" *                                 Section D                                              *"<<endl;
-   cout <<" *                                Roll no: 20                                             *"<<endl;
-   cout <<" *                                                                                        *"<<endl;
-   cout <<" *                                                                                        *"<<endl;
-   cout <<" *                                                                                        *"<<endl;
-   cout <<" *                                                                                        *"<<endl;
-   cout <<" *                                                                                        *"<<endl;
-   cout <<" ******************************************************************************************"; 
+    cout<<"************************************************************************"<<endl;
+    cout<<"*                                                                      *"<<endl;
+    cout<<"*                                                                      *"<<endl;
+    cout<<"*                    JAWAD ALI AMJAD                                   *"<<endl;
+    cout<<"*                     ROLL NO:183                                      *"<<endl;
+    cout<<"*                      SECTION:D                                       *"<<endl;
+    cout<<"*                                                                      *"<<endl;
+    cout<<"*                                                                      *"<<endl;
+    cout<<"*                                                                      *"<<endl;
+    cout<<"*                                                                      *"<<endl;
+    cout<<"*                                                                      *"<<endl;
+    cout<<"************************************************************************";
 
 
 }
